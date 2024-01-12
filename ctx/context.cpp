58899e409d7f47fd1307fade172eb86cb4ea6167
Sanzhar <sanzhar.maratov@proton.me>
2024-01-13 03:47:27 +0600

    move stack when setting up a context

--- ctx/context.cpp
+++ ctx/context.cpp
@@ -2,7 +2,7 @@
 
 void Context::Setup(MutableMemView stack, ITrampoline *trampoline) {
     trampoline_ = trampoline;
-    hw_.Setup(stack, this);
+    hw_.Setup(std::move(stack), this);
 }
 
 void Context::SwitchTo(Context &target) { hw_.SwitchTo(target.hw_); }
